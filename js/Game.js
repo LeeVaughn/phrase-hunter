ad62f8c0bb8ea84fd11f519a96012dad78ec82f8
LeeVaughn <dlvprogramming@gmail.com>
2019-01-13 16:27:45 -0800

    created createPhrases method

--- js/Game.js
+++ js/Game.js
@@ -4,7 +4,22 @@
 class Game {
   constructor() {
     this.missed = 0;
-    this.phrases = [];
+    this.phrases = this.createPhrases();
     this.activePhrase = null;
+  }
+/**
+ * creates phrases for use in game
+ * @return {array} an array of phrases to be used in the game
+ */
+  createPhrases() {
+    const phrases = [
+      {phrase: "may the force be with you"},
+      {phrase: "show me the money"},
+      {phrase: "i see dead people"},
+      {phrase: "my precious"},
+      {phrase: "do or do not"}
+    ]
+
+    return phrases;
   }
 }
