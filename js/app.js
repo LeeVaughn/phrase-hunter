d5962193677121f98cc36348ed889c7b1a402629
LeeVaughn <dlvprogramming@gmail.com>
2019-01-15 14:43:17 -0800

    added event listener to phrase

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 let game;
 
 // listens for clicks on Start Game button, instantiates a new Game object and calls startGame on it
-$("#btn__reset").click(function() {
+$("#btn__reset").click(() => {
   game = new Game();
 
   game.startGame();
+});
+
+// listens for clicks on the displayed phrase and calls checkLetter passing clicked letter as a parameter
+$("#phrase").on("click", "li", (e) => {
+  const letter = e.target.innerText;
+
+  game.activePhrase.checkLetter(letter);
 });
 
 
