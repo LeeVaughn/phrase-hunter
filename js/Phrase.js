505954a23eb764536a892db20aa1d4493203e822
LeeVaughn <dlvprogramming@gmail.com>
2019-01-19 10:51:14 -0800

    updated comments on checkLetter method

--- js/Phrase.js
+++ js/Phrase.js
@@ -23,6 +23,8 @@ class Phrase {
   /**
    * checks if passed letter is in phrase
    * @param (string) letter - letter to check
+   * @return {boolean} true if phrase includes letter, false if it doesn't
+won
    */
   checkLetter(letter) {
     if (this.phrase.includes(letter)) {
