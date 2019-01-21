7e2772e8916347c8cda82c82caa85b041a54a5a4
LeeVaughn <dlvprogramming@gmail.com>
2019-01-21 01:17:10 -0800

    added keyup listener

--- js/app.js
+++ js/app.js
@@ -10,8 +10,16 @@ $("#btn__reset").click(() => {
 // listens for clicks on the onscreen keyboard and calls handleInteraction method
 $("#qwerty").on("click", "button", (e) => {
   const button = e.target;
+  const letter = button.innerText;
 
-  game.handleInteraction(button);
+  game.handleInteraction(button, letter);
+});
+
+$(document).keyup((e) => {
+  const letter = e.key;
+  const button = $(`button:contains(${letter})`);
+
+  game.handleInteraction(button, letter);
 });
 
 
